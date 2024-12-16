092e4bf7423b747e6e0bb6571f59f54935bb0fd8
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-12-16 19:08:56 -0300

    contenido perfil

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
 
         </div>
         
-        <Boton to="/portfolio">Ver Portfolio</Boton>
+        <Boton to="/portfolio">Ver Perfil</Boton>
          
       
         
--- src/Components/Portolio/Portfolio.jsx
+++ src/Components/Portolio/Portfolio.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import Boton from '../Boton/Boton'
+import foto2 from '../../Image/segundoleo.png'
+import bombilla from '../../Image/bombilla.png'
+import globo from '../../Image/globo.png'
+import grupo from '../../Image/grupo.png'
+import "./Portfolio.css"
 
 const Portfolio = () => {
   return (
-    <div>
-        <h2>Portfolio</h2>
-        <Boton to="/habilidades">Habilidades</Boton>
+    <div className='hidden'>
+        <div className="row principal2">
+
+         <div className="col-md-12 col-lg-5 containerSedungo ">
+            <img src={foto2} alt="foto de leo" className='segundoleo'/>
+            <button className='botonDescargar'>Descargar Cv</button>
+          </div>
+        
+          <div className="col-md-12 col-lg-7 textoContainer2 ">
+
+            <div className='refer'>
+              <img src={globo} alt="globo" className='logo' />
+              <p className='textoRefer'>Más de 10 Años de Experiencia</p>
+            </div>
+
+            <div className='refer'>
+              <img src={bombilla} alt="bombilla" className='logo' />
+              <p className='textoRefer'>Logística y Planificación</p>
+            </div>
+
+            <div className='refer'>
+              <img src={grupo} alt="grupo" className='logo' />
+              <p className='textoRefer'>Coordinación de Equipos</p>
+            </div>
+
+            
+
+            <div className='description'>
+              <p className='descriptionP'>He tenido la oportunidad de trabajar en diversos países, desempeñándome en áreas como la gestión de eventos, espectáculos y producción de giras artísticas, así como en el sector de turismo. Estas experiencias, en distintos contextos y rubros, me han permitido desarrollar una sólida capacidad en atención al público y resolución de problemas.</p>
+              <p className='descriptionP'>Además, mi experiencia en diseño gráfico ha sido un recurso clave para todos los proyectos en los que he trabajado. Desde la creación de materiales visuales hasta algo más actual en mi carrera, como es la implementación de soluciones digitales basadas en el desarrollo de software, mi enfoque ha estado siempre orientado a crear experiencias integradas que potencien la comunicación, la publicidad y la presencia digital.</p>
+            </div>
+
+          </div>
+
+          
+
+        </div>
+        <div className='decideBoton'>
+          <Boton to="/">Inicio</Boton>
+        </div>
     </div>
   )
 }
